856c4d4251eabc1441df3a71b1d0871a99a4a255
Mariappan05 <rammariappan05@gmail.com>
2024-09-15 15:11:51 +0530

    Changes on updateemployeedetails and employeeDto entities also

--- FixitAPI/Models/EmployeeDto.cs
+++ FixitAPI/Models/EmployeeDto.cs
@@ -4,7 +4,7 @@
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Age { get; set; }
+        public int? Age { get; set; }
         public string Gender { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
@@ -12,8 +12,7 @@
         public string State { get; set; }
         public string PhoneNumber { get; set; }
         public List<string> Roles { get; set; }
-        public string Experience { get; set; }
-        public List<int> ServiceIds { get; set; }
-
+        public int? Experience { get; set; }
     }
+
 }
